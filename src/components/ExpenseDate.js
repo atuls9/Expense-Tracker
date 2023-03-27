2e93d3265edec01d32215d4c85caac9e876f2cd2
atuls9 <atul.sooryawanshi@gmail.com>
2023-03-28 01:24:49 +0530

    create card element

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,14 @@
+import './ExpenseDate.css'
+
 function ExpenseDate(props) {
   const month = props.data.date.toLocaleString("en-US", { month: "long" });
   const day = props.data.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.data.date.getFullYear();
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,15 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
+import Card from "./Card";
+
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2022,4,20);
-  // const expenseTitle = 'Car Insurance';
-  // const expenseAmount = 20000;
-  // const LocationOfExpenditure = 'Mumbai'
-
   return (
-    <div className="expense-item">
-      <ExpenseDate data = {props.data}></ExpenseDate>
-      <ExpenseDetails data = {props.data}></ExpenseDetails>
-    </div>
+    <Card className="expense-item">
+      <ExpenseDate data={props.data}></ExpenseDate>
+      <ExpenseDetails data={props.data}></ExpenseDetails>
+    </Card>
   );
 }
 
