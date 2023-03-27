e303308ae9ec5562c37bb301a67aa9bda376b4ae
atuls9 <atul.sooryawanshi@gmail.com>
2023-03-27 14:39:25 +0530

    Passing data using map method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 20000,
+      location: "Mumbai",
+      date: new Date(2022, 4, 20),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 74000,
+      location: "Hyderabad",
+      date: new Date(2022, 6, 8),
+    },
+    {
+      id: "e3",
+      title: "Movie",
+      amount: 2000,
+      location: "Mumbai",
+      date: new Date(2022, 7, 8),
+    },
+    {
+      id: "e4",
+      title: "Bike",
+      amount: 90000,
+      location: "Pune",
+      date: new Date(2022, 1, 25),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {
+      expenses.map((item,i)=>
+      <ExpenseItem data= {item}></ExpenseItem> )
+      
+      }
+      
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-const expenseDate = new Date(2022,4,20);
-const expenseTitle = 'Car Insurance';
-const expenseAmount = 20000;
-const LocationOfExpenditure = 'Mumbai'
+// const expenseDate = new Date(2022,4,20);
+// const expenseTitle = 'Car Insurance';
+// const expenseAmount = 20000;
+// const LocationOfExpenditure = 'Mumbai'
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.data.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <h2>{LocationOfExpenditure}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.data.title}</h2>
+        <h2>{props.data.location}</h2>
+        <div className="expense-item__price">{props.data.amount}</div>
       </div>
     </div>
   );
