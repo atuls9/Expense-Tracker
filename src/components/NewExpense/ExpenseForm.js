ab44bbda55888416596aef1cd861dded098759cc
atuls9 <atul.sooryawanshi@gmail.com>
2023-03-29 18:23:59 +0530

    on submit form create an object and log on console

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -55,8 +55,17 @@ const ExpenseForm = () => {
 
     // })
   };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newExpense = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(newExpense);
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
